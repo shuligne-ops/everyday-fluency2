f67117aea992614a4742bd8bdf462b854e99306d
shuligne-ops <shuligne@gmail.com>
2026-09-22 10:20:32 +0700

    Add plain-language result explanation

--- src/app/retrieval-lab/page.tsx
+++ src/app/retrieval-lab/page.tsx
@@ -1,1 +1,11 @@
-export { default } from './DeepPractice'
+import DeepPractice from './DeepPractice'
+import ResultClarityLayer from './ResultClarityLayer'
+
+export default function RetrievalLabPage() {
+  return (
+    <>
+      <ResultClarityLayer />
+      <DeepPractice />
+    </>
+  )
+}
